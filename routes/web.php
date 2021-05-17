475ed1579c2c1a21d318eea03a6d754cf1d485d6
Renato Andrade <renatorobot@outlook.com.br>
2021-05-17 11:17:13 -0300

    rotas

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use App\Cliente;
+
+Route::get('/clientes', function () {
+    
+    $clientes =  Cliente::all();
+    foreach($clientes as $c){
+        echo "<p>ID: " . $c->id . "</p>";
+        echo "<p>Nome: " . $c->nome . "</p>";
+        echo "<p>Telefone: " . $c->telefone . "</p>";
+        echo "<hr>";
+    }
+
+});
